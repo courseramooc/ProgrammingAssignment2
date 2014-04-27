e0fab0cb4fa071e5f4df9783152959e10f951738
Mitch <sudoharbinger@gmail.com>
2014-04-26 21:40:48 -0300

    Required changes applied to skeleton script

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,46 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This script caches the expensive calculation of inverting a matrix. If the
+## particular inverted matrix is not cached, the calculation is performed.
+## Otherwise the cached inverted matrix is returned
 
-## Write a short comment describing this function
+## This function takes a matrix as argument and returns a list with four 
+## functions to manipulate the given matrix
 
 makeCacheMatrix <- function(x = matrix()) {
+    im <- NULL
+
+    set <- function(y) {
+        x <<- y
+        im <<- NULL
+    }
+
+    get <- function() x
+
+    setinversematrix <- function(solve) im <<- solve
+
+    getinversematrix <- function() im
+
+    list(set=set, get=get, 
+        setinversematrix=setinversematrix,
+        getinversematrix=getinversematrix)
 
 }
 
 
-## Write a short comment describing this function
+## This function takes a matrix as argument and returns its inverse. If the 
+## inverse is previously cached, it returns the cached inverse. Otherwise
+## it calculates the inverse, caches it and returns the inverted matrix
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+	## Return a matrix that is the inverse of 'x'
+	im <- x&getinversematrix()
+
+	if(!is.null(im)) {
+		message("getting cached data")
+		return(im)
+	}
+
+	data <- x$get()
+	im <- inversematrix(data, ...)
+	x$setinversematrix(im)
+	im
 }
